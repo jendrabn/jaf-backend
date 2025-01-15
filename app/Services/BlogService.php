db2e307282367293789d9ead23ae663ed08029f2
jendrabn <jendra455@gmail.com>
2025-01-15 13:46:38 +0700

    Add blog list sorting

--- app/Services/BlogService.php
+++ app/Services/BlogService.php
@@ -8,9 +8,11 @@ use Illuminate\Http\Request;
 class BlogService
 {
 
-    public function getBlogs(Request $request)
+    public function getBlogs(Request $request, int $size = 10)
     {
-        $blogs = Blog::published();
+        $page = $request->get('page', 1);
+
+        $blogs = Blog::with(['media', 'category', 'tags'])->published();
 
         $blogs->when(
             $request->has('category_id'),
@@ -29,7 +31,21 @@ class BlogService
                 ->orWhereHas('tags', fn($q) => $q->where('name', 'like', "%{$request->get('search')}%"))
         );
 
-        $blogs = $blogs->latest('id')->paginate(10);
+        $blogs->when(
+            $request->has('sort_by'),
+            function ($q) use ($request) {
+                $sorts = [
+                    'newest' => ['id', 'desc'],
+                    'oldest' => ['id', 'asc'],
+                    'views' => ['views_count', 'desc']
+                ];
+
+                return $q->orderBy(...$sorts[$request->get('sort_by') ?? $sorts['newest']]);
+            },
+            fn($q) => $q->orderBy('id', 'desc')
+        );
+
+        $blogs = $blogs->paginate(perPage: $size, page: $page);
 
         return $blogs;
     }
