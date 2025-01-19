b949441a534b15f416117d83c0c1b452c7f76b90
jendrabn <jendra455@gmail.com>
2025-01-19 19:44:21 +0700

    Cache response raja ongkir fetch costs

--- app/Services/RajaOngkirService.php
+++ app/Services/RajaOngkirService.php
@@ -4,47 +4,55 @@ namespace App\Services;
 
 use App\Models\Shipping;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Cache;
 use Symfony\Component\HttpFoundation\Response;
 
 class RajaOngkirService
 {
-  public function getCosts(int $destination, int $weight, array $couriers = Shipping::COURIERS): array
-  {
-    $costs = [];
-    foreach ($couriers as $courier) {
-      $costs = array_merge($costs, $this->fetchCosts($destination, $weight, $courier));
+    public function getCosts(int $destination, int $weight, array $couriers = Shipping::COURIERS): array
+    {
+        $costs = [];
+        foreach ($couriers as $courier) {
+            $costs = array_merge($costs, $this->fetchCosts($destination, $weight, $courier));
+        }
+
+        return $costs;
     }
 
-    return $costs;
-  }
+    public function getService(string $service, int $destination, int $weight, string $courier): array|null
+    {
+        $costs = $this->fetchCosts($destination, $weight, $courier);
 
-  public function getService(string $service, int $destination, int $weight, string $courier): array|null
-  {
-    $costs = $this->fetchCosts($destination, $weight, $courier);
+        return collect($costs)->firstWhere('service', $service);
+    }
 
-    return collect($costs)->firstWhere('service', $service);
-  }
+    public function fetchCosts(int $destination, int $weight, string $courier): array
+    {
+        $cacheKey = "shipping_costs_{$destination}_{$weight}_{$courier}";
 
-  public function fetchCosts(int $destination, int $weight, string $courier): array
-  {
-    $baseUrl = config('shop.rajaongkir.base_url');
-    $key =  config('shop.rajaongkir.key');
-    $origin = config('shop.address.city_id');
+        return Cache::remember($cacheKey, 60 * 60, function () use ($destination, $weight, $courier) {
+            $baseUrl = config('shop.rajaongkir.base_url');
+            $key = config('shop.rajaongkir.key');
+            $origin = config('shop.address.city_id');
 
-    $response = Http::acceptJson()
-      ->withHeader('key', $key)
-      ->post("$baseUrl/cost", compact('origin', 'destination', 'weight', 'courier'))
-      ->throwUnlessStatus(Response::HTTP_OK);
+            $response = Http::acceptJson()
+                ->withHeader('key', $key)
+                ->post("$baseUrl/cost", compact('origin', 'destination', 'weight', 'courier'));
 
-    $results = $response->json('rajaongkir.results')[0];
+            if ($response->status() !== Response::HTTP_OK) {
+                throw new \Exception('Failed to fetch costs');
+            }
 
-    return collect($results['costs'] ?? [])->map(fn ($cost) => [
-      'courier' => $courier,
-      'courier_name' => $results['name'],
-      'service' => $cost['service'],
-      'service_name' => $cost['description'],
-      'cost' => $cost['cost'][0]['value'],
-      'etd' => str_replace(['hari', ' '], '', strtolower($cost['cost'][0]['etd'])) . ' hari',
-    ])->toArray();
-  }
+            $results = $response->json('rajaongkir.results')[0];
+
+            return collect($results['costs'] ?? [])->map(fn($cost) => [
+                'courier' => $courier,
+                'courier_name' => $results['name'],
+                'service' => $cost['service'],
+                'service_name' => $cost['description'],
+                'cost' => $cost['cost'][0]['value'],
+                'etd' => str_replace(['hari', ' '], '', strtolower($cost['cost'][0]['etd'])) . ' hari',
+            ])->toArray();
+        });
+    }
 }
