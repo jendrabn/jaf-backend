05d74fa5116fb13b3f61910f0e8c0d8ec757c1e6
jendrabn <jendra455@gmail.com>
2025-01-24 07:36:12 +0700

    Add payment method ewallets

---
+++ app/Http/Resources/EwalletResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class EwalletResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'account_name' => $this->account_name,
+            'account_username' => $this->account_username,
+            'phone' => $this->phone,
+            'logo' => $this->logo?->getUrl()
+        ];
+    }
+}
--- app/Http/Resources/OrderDetailResource.php
+++ app/Http/Resources/OrderDetailResource.php
@@ -7,33 +7,34 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class OrderDetailResource extends JsonResource
 {
-  /**
-   * Transform the resource into an array.
-   *
-   * @return array<string, mixed>
-   */
-  public function toArray(Request $request): array
-  {
-    return [
-      'id' => $this->id,
-      'items' => OrderItemResource::collection($this->items),
-      'invoice' => InvoiceResource::make($this->invoice),
-      'payment' => PaymentResource::make($this->invoice->payment),
-      'shipping_address' => $this->shipping->address,
-      'shipping' => ShippingResource::make($this->shipping),
-      'notes' => $this->notes,
-      'cancel_reason' => $this->cancel_reason,
-      'status' => $this->status,
-      'total_quantity' => $this->total_quantity,
-      'total_weight' => $this->shipping->weight,
-      'total_price' => $this->total_price,
-      'shipping_cost' => $this->shipping_cost,
-      'total_amount' => $this->invoice->amount,
-      'payment_due_date' => $this->invoice->due_date,
-      'confirmed_at' => $this->confirmed_at,
-      'completed_at' => $this->completed_at,
-      'cancelled_at' => $this->cancelled_at,
-      'created_at' => $this->created_at,
-    ];
-  }
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'items' => OrderItemResource::collection($this->items),
+            'invoice' => InvoiceResource::make($this->invoice),
+            'payment' => PaymentResource::make($this->invoice->payment),
+            'shipping_address' => $this->shipping->address,
+            'shipping' => ShippingResource::make($this->shipping),
+            'notes' => $this->notes,
+            'cancel_reason' => $this->cancel_reason,
+            'status' => $this->status,
+            'total_quantity' => $this->total_quantity,
+            'total_weight' => $this->shipping->weight,
+            'total_price' => $this->total_price,
+            'shipping_cost' => $this->shipping_cost,
+            'total_amount' => $this->invoice->amount,
+            'payment_due_date' => $this->invoice->due_date,
+            'confirmed_at' => $this->confirmed_at,
+            'shipped_at' => $this->shipping->created_at,
+            'completed_at' => $this->completed_at,
+            'cancelled_at' => $this->cancelled_at,
+            'created_at' => $this->created_at,
+        ];
+    }
 }
