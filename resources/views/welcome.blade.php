42d61626f6eec281a02970ccefc681bc36306762
jendrabn <jendra455@gmail.com>
2025-09-29 19:45:24 +0700

    fix: fix style path

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.13.1/font/bootstrap-icons.min.css"
           rel="stylesheet" />
 
-    @vite('resources/scss/adminlte.scss')
+    @vite('resources/scss/style.scss')
 </head>
 
 <body>
